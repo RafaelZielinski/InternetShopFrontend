6f8df90bb130c308b64159f268a50b5862d71276
RafaelZielinski <rafekzielinski@wp.pl>
2022-12-09 00:01:01 +0100

    adding working basket

--- src/app/modules/cart/cart.service.ts
+++ src/app/modules/cart/cart.service.ts
@@ -8,15 +8,23 @@ import { CartSummaryItem } from './model/cartSummaryItem';
   providedIn: 'root'
 })
 export class CartService {
-
+  
   constructor(private http: HttpClient
-              ) { }
+    ) { }
+    
+    getCart(id: number): Observable<CartSummary> {
+      return this.http.get<CartSummary>("/api/carts/" + id);
+    }
+    
+    addToCart(id: number, cartItem: any): Observable<CartSummary>{
+      return this.http.put<CartSummary>("/api/carts/" + id, cartItem)
+    }
 
-  getCart(id: number): Observable<CartSummary> {
-    return this.http.get<CartSummary>("/api/carts/" + id);
-  }
+    updateCart(id: number, items: any[]): Observable<CartSummary> {
+      return this.http.put<CartSummary>("/api/carts/" + id + "/update", items);
+    }
 
-  addToCart(id: number, cartItem: any): Observable<CartSummary>{
-    return this.http.put<CartSummary>("/api/carts/" + id, cartItem)
+    deleteCartItem(itemId: number): Observable<void>{
+        return this.http.delete<void>("/api/cartItems/" + itemId);
+    }
   }
-}
